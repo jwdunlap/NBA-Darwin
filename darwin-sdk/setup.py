0d4e407930e5f5e302c103eb076ab5905c48643f
Dong Hyon Gu <donghyon.gu@utexas.edu>
2019-04-10 20:52:51 -0500

    added darwin sdk and initial notebook

---
+++ darwin-sdk/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+from os import path
+
+setup(
+    name='amb_sdk',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='1.0',
+
+    description='The AMB SDK is a python client for the AMB API.',
+
+    # The project's main homepage.
+    url='https://bitbucket.org/sparkcognition/amb-sdk',
+
+    # Author details
+    author='Sheila Cheng',
+    author_email='scheng@sparkcognition.com',
+    install_requires=[
+        'validators',
+        'urllib3',
+        'pandas',
+        'requests_toolbelt',
+        'requests',
+        'matplotlib',
+        'scikit-learn',
+        'scipy',
+        'jupyter'
+    ],
+    packages=['amb_sdk']
+)
